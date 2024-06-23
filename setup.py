c3aa9be6cd5ba835debe85f487464f432f15f364
Emads <emadsaab722@gmail.com>
2024-06-23 17:35:00 +0300

    fix a small potentiel bug by using self._song in fetching lyrics method to stay consistent

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as file:
 
 setup(
     name='eminem_lyric',
-    version='1.0.5',
+    version='1.0.6',
     packages=find_packages(),
     description='A Python package for fetching Eminem song lyrics.',
     long_description=long_description,  # Use README.md content as long description
