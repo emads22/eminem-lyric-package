d44ed5837f82222335d52998dafb2b7fae28190b
Emads <emadsaab722@gmail.com>
2024-07-07 09:22:44 +0300

    adjust example_usage.py file

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -9,11 +9,11 @@ def main():
     try:
         # Fetch the formatted lyrics of the song
         lyrics = eminem_lyric.lyric
-        print(f"Lyrics for '{song_title}':\n{lyrics}")
+        print(f"\n\n>> Lyrics for '{song_title}':\n\n{lyrics}\n")
 
         # Fetch the raw lyrics data (as a dictionary)
         raw_lyrics = eminem_lyric.lyric_raw
-        print(f"\nRaw lyrics data for '{song_title}':\n{raw_lyrics}")
+        print(f"\n\n>> Raw lyrics data for '{song_title}':\n\n{raw_lyrics}\n\n")
 
     except Exception as e:
         print(f"An error occurred: {e}")
